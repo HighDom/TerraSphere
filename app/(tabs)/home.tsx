09a0a403774a982539346e788205c9a72b975558
Dominique Buob <82541879+HighDom@users.noreply.github.com>
2024-07-28 14:07:03 +0200

    fixed login-issues

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import { FlatList, GestureHandlerRootView } from 'react-native-gesture-handler'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <GestureHandlerRootView>
+      <SafeAreaView>
+        <FlatList 
+          data={[{id: 1}]} 
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => (
+            <Text>{item.id}</Text>
+          )}
+        />
+          
+
+      </SafeAreaView>
+    </GestureHandlerRootView>
   )
 }
 
