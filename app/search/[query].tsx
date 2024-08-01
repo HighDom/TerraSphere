d1abfedbe53b1a9c91a6b76a9d5294deaa28ff2f
Dominique Buob <82541879+HighDom@users.noreply.github.com>
2024-08-01 09:17:02 +0200

    queue update

--- app/search/[query].tsx
+++ app/search/[query].tsx
@@ -1,67 +1,67 @@
-import { View, Text, SafeAreaView, Image, Alert } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { FlatList, GestureHandlerRootView, RefreshControl } from 'react-native-gesture-handler'
-import { images } from '@/constants'
-import SearchInput from '@/components/SearchInput'
-import Trending from '@/components/Trending'
-import EmptyState from '@/components/EmptyState'
-import useAppwrite from '@/lib/useAppwrite'
-import { getAllPosts, getLatestPosts, searchPosts } from '@/lib/appwrite'
-import VideoCard from '@/components/VideoCard'
-import { useLocalSearchParams } from 'expo-router'
+import { useEffect } from "react";
+import { useLocalSearchParams } from "expo-router";
+import { View, Text, FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import useAppwrite from "../../lib/useAppwrite";
+import { searchPosts } from "../../lib/appwrite";
+import VideoCard from "@/components/VideoCard";
+import SearchInput from "@/components/SearchInput";
+import EmptyState from "@/components/EmptyState";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 
 const Search = () => {
-  const {query} = useLocalSearchParams();
-  const {data: posts = [] as any[], refetch } = useAppwrite(searchPosts(query));
+  const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query)) as { data: any[], refetch: () => Promise<void> };
 
   useEffect(() => {
     refetch();
   }, [query]);
 
-
   return (
     <GestureHandlerRootView>
-      <SafeAreaView className='bg-primary h-full'>
-        <FlatList 
-          data={posts} 
+      <SafeAreaView className="bg-primary h-full">
+        <FlatList
+          data={posts}
           keyExtractor={(item) => item.$id}
-          renderItem={({item}) => (
-            <VideoCard video={item} />
+          renderItem={({ item }) => (
+            <VideoCard
+              video={{
+                title: item.title,
+                thumbnail: item.thumbnail,
+                video: item.video,
+                creator: {
+                  username: item.creator,
+                  avatar: item.avatar,
+                },
+              }}
+            />
           )}
           ListHeaderComponent={() => (
-            <View className='my-6 px-4 space-y-4'>
-                  <Text className="font-pmedium text-sm text-gray-100">
-                    Search Results
-                  </Text>
-                  <Text className="text-2xl font-psemibold text-white">
-                    {query}
-                  </Text>
-                  <SearchInput 
-                    initialQuery={query}
-                    title={''} 
-                    value={''} 
-                    handleChangeText={function (text: string): void {
-                      throw new Error('Function not implemented.')
-                  } } 
-                otherStyles={''} 
-                 placeHolder={''}
-                />
-            </View>
-          )}
+            <>
+              <View className="flex my-6 px-4">
+                <Text className="font-pmedium text-gray-100 text-sm">
+                  Search Results
+                </Text>
 
+                <View className="mt-6 mb-8">
+                  <SearchInput initialQuery={query}/>
+                </View>
+              </View>
+            </>
+          )}
           ListEmptyComponent={() => (
-            <View className='flex-1 justify-center items-center'>
-              <EmptyState 
-                title='No Videos Found' 
-                subTitle='Be the first one to Upload a video'              />
-            </View>
+            <EmptyState
+              title="No Videos Found"
+              subtitle="No videos found for this search query"
+            />
           )}
         />
-          
-
       </SafeAreaView>
     </GestureHandlerRootView>
-  )
-}
 
-export default Search
+  );
+};
+
+export default Search;
